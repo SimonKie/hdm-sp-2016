a1d06c5a0e2bf1c507262788bb428347c0646b5f
SimonKie <simon.kienzler@gmx.de>
2016-07-28 21:02:22 +0200

    changed font

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 
   /*
    * index file of the invoice data to PDF converter
-   * created at the Stuttgart Media Uni in summer 2016. 
+   * created at the Stuttgart Media University in summer 2016. 
    */
    
    $title = "HdM Summer Project 2016";
    
    echo "<!doctype html>";
-   echo "<html lang='de'><head><meta charset='utf-8' /><title>" . $title . "</title></head>";
+   echo "<html lang='de'><head>";
+   echo "<meta charset='utf-8' /><title>" . $title . "</title>";
+   echo "<style><!-- * {font-family: 'Open Sans', Calibri, sans-serif;} --></style>";
+   echo "</head>";
    echo "<body><h1>" . $title . "</h1>";
    echo "<p><strong>Date:</strong> " . date('d.m.Y') . "</p>";
    echo "</body></html>";
